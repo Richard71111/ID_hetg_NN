24979fb1b438c5eed36633b5947aa9c8b381a1db
RichardSui <112217353+Richard71111@users.noreply.github.com>
2025-11-19 15:29:21 -0500

    Delete train directory

--- train/MLP_TrainTest/__init__.py
+++
@@ -1,4 +0,0 @@
-from .MLP_train import *
-from .MLP_test import *
-
-__all__ = ['train_MLP', 'train_MLP_epoch', 'validate_MLP_epoch', 'test_MLP_autoregression']
--- train/__init__.py
+++
@@ -1,2 +0,0 @@
-from .MLP_TrainTest import *
-__all__ = ['train_MLP', 'train_MLP_epoch', 'validate_MLP_epoch', 'test_MLP_autoregression']
